5b0c989df7f3a67f47d9abd35e5d729b3b6c6123
Matthew O'Connell <totallyterrific@tutanota.com>
2025-10-08 11:15:41 +0200

    fix(ToCsv): default to "," as separator

--- src/YoloAbstractions/Extensions/ToCsv.cs
+++ src/YoloAbstractions/Extensions/ToCsv.cs
@@ -4,5 +4,5 @@ namespace YoloAbstractions.Extensions;
 
 public static partial class EnumerableExtensions
 {
-    public static string ToCsv<T>(this IEnumerable<T> enumerable) => string.Join(", ", enumerable);
+    public static string ToCsv<T>(this IEnumerable<T> enumerable, string? separator = ",") => string.Join(separator, enumerable);
 }
