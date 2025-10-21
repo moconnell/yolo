c80ede8947854296bea57aa411825d2f930d8f27
Matthew O'Connell <totallyterrific@tutanota.com>
2025-10-21 21:13:08 +0200

    test(RobotWealthConfigExtensionsTest): add ext. methods tests

---
+++ test/Robotwealth.Api.Test/RobotWealthConfigExtensionsTest.cs
@@ -0,0 +1,48 @@
+using Microsoft.Extensions.Configuration;
+using RobotWealth.Api.Extensions;
+using Shouldly;
+
+namespace RobotWealth.Api.Test;
+
+public class RobotWealthConfigExtensionsTest
+{
+    [Theory]
+    [InlineData(false)]
+    [InlineData(true)]
+    public void GivenGoodConfig_ShouldReturnHasRobotWealthConfig(bool hasRwConfig)
+    {
+        // arrange
+        var builder = new ConfigurationBuilder();
+        if (hasRwConfig)
+        {
+            builder.AddJsonFile("appsettings.json");
+            builder.AddJsonFile($"appsettings.local.json");
+        }
+
+        var config = builder.Build();
+
+        // act
+        var result = config.HasRobotWealthConfig();
+
+        // assert
+        result.ShouldBe(hasRwConfig);
+    }
+
+    [Fact]
+    public void GivenGoodConfig_ShouldGetRobotWealthConfig()
+    {
+        // arrange
+        var builder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", true, true)
+            .AddJsonFile($"appsettings.local.json", true, true);
+        var config = builder.Build();
+
+        // act
+        var unravelConfig = config.GetRobotWealthConfig();
+
+        // assert
+        unravelConfig.ShouldNotBeNull();
+        unravelConfig.VolatilitiesUrlPath.ShouldNotBeNullOrEmpty();
+        unravelConfig.WeightsUrlPath.ShouldNotBeNullOrEmpty();
+    }
+}
