b3c10a9bba57d3192260d85527b731eec83cce90
Matthew O'Connell <totallyterrific@tutanota.com>
2025-11-02 20:57:35 +0100

    fix(RobotWealthFactorService): validate returned dates

--- src/RobotWealth.Api/RobotWealthFactorService.cs
+++ src/RobotWealth.Api/RobotWealthFactorService.cs
@@ -28,11 +28,11 @@ public class RobotWealthFactorService : IGetFactors
         var weights = await _apiService.GetWeightsAsync(cancellationToken);
         if (!weights.Any())
             throw new ApiException("No weights returned");
-        
+
         var volatilities = await _apiService.GetVolatilitiesAsync(cancellationToken);
         if (!volatilities.Any())
             throw new ApiException("No volatilities returned");
-        
+
         var factorDataFrame = ToFactorDataFrame(weights, volatilities);
 
         return factorDataFrame;
@@ -42,6 +42,12 @@ public class RobotWealthFactorService : IGetFactors
         IReadOnlyList<RwWeight> weights,
         IReadOnlyList<RwVolatility> volatilities)
     {
+        var weightsDate = weights[0].Date;
+        if (weights.Any(w => w.Date != weightsDate))
+            throw new ApiException("Inconsistent dates in weights");
+        if (volatilities.Any(v => v.Date != weightsDate))
+            throw new ApiException("Volatilities date does not match weights date");
+
         var orderedWeights = weights.OrderBy(x => x.Ticker).ToArray();
         var weightsTickers = orderedWeights.Select(w => w.Ticker).ToArray();
         var carryFrame = orderedWeights.Select(w => w.CarryMegafactor).ToArray();
@@ -57,7 +63,7 @@ public class RobotWealthFactorService : IGetFactors
 
         return FactorDataFrame.NewFrom(
             weightsTickers,
-            weights[0].Date,
+            weightsDate,
             (FactorType.Carry, carryFrame),
             (FactorType.Momentum, momentumFrame),
             (FactorType.Trend, trendFrame),
